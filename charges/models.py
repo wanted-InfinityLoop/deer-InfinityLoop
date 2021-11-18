a1f8478de8d6d780f321ee534334bb9696638598
Oderystella <ddott59@gmail.com>
2021-11-19 00:36:56 +0900

    feat: add models schema and migration files
    - 유저, 비히클, 지역, 요금 모델 작성
    - 마이그레이션 파일 추가
    - geo 타입 관련 gdal 라이브러리 설치 및 설정파일 추가

--- charges/models.py
+++ charges/models.py
@@ -1,3 +1,12 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Charge(models.Model):
+    id                = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    rental_charge     = models.SmallIntegerField()
+    additional_charge = models.SmallIntegerField()
+
+    class Meta:
+            db_table = "charges"
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,26 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    id           = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    deer_name    = models.CharField(max_length=32)
+    service_area = models.ForeignKey("areas.ServiceArea", on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "vehicles"
+
+
+class Usage(models.Model):
+    id            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    vehicle       = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    end_lat       = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    end_lng       = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    start_at      = models.DateTimeField(auto_now_add=True)    
+    end_at        = models.DateTimeField(null=True)    
+    charge_amount = models.IntegerField(null=True)
+    user          = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "usages"
