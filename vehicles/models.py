9776d2b2c9c9d4305587e7fa404e1223b4d99108
Oderystella <ddott59@gmail.com>
2021-11-19 12:17:01 +0900

    fix: modify foreignkey on_delete option and migrate
    - 테이블의 foreignkey on_delete 옵션 수정

--- vehicles/models.py
+++ vehicles/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Vehicle(models.Model):
     id           = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     deer_name    = models.CharField(max_length=32)
-    service_area = models.ForeignKey("areas.ServiceArea", on_delete=models.PROTECT)
+    service_area = models.ForeignKey("areas.ServiceArea", null=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "vehicles"
@@ -14,13 +14,13 @@ class Vehicle(models.Model):
 
 class Usage(models.Model):
     id            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    vehicle       = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    vehicle       = models.ForeignKey(Vehicle, null=True, on_delete=models.SET_NULL)
     end_lat       = models.DecimalField(max_digits=8, decimal_places=6, null=True)
     end_lng       = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     start_at      = models.DateTimeField(auto_now_add=True)    
     end_at        = models.DateTimeField(null=True)    
-    charge_amount = models.IntegerField(null=True)
-    user          = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    charge_amount = models.PositiveIntegerField(default=0)
+    user          = models.ForeignKey("users.User", null=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "usages"
