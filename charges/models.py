0b3a1d35e56bd92aa68715f72af77214bd1b41d7
YeJun <hayejun1013@naver.com>
2021-11-19 17:30:20 +0900

    feat: add discount or penalties models and migrate files
    - 할인, 벌금 모델링 추가
    - 마이그레이션 파일 추가

--- charges/models.py
+++ charges/models.py
@@ -10,3 +10,31 @@ class Charge(models.Model):
 
     class Meta:
             db_table = "charges"
+
+class Unit(models.Model):
+    id   = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=16)
+
+    class Meta:
+            db_table = "units"
+
+class Type(models.Model):
+    class Type(models.IntegerChoices):
+        DISCOUNT = 1
+        PENALTY  = 2
+
+    id   = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=16)
+
+    class Meta:
+            db_table = "types"
+
+class DiscountOrPenalties(models.Model):
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number      = models.PositiveIntegerField()
+    description = models.CharField(max_length=32)
+    unit        = models.ForeignKey(Unit, on_delete=models.PROTECT)
+    type        = models.ForeignKey(Type, on_delete=models.PROTECT)
+    
+    class Meta:
+            db_table = "discount_or_penalties"
