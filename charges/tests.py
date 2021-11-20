3d985110024b95d02bc74a771140286f5a0efb52
YeJun <hayejun1013@naver.com>
2021-11-20 23:11:24 +0900

    feat: add penalty charge post view function
    - 벌금요금 추가 기능 구현
    - 벌금요금 추가 기능 테스트 코드 구현

--- charges/tests.py
+++ charges/tests.py
@@ -247,7 +247,7 @@ class DiscountViewTest(TestCase):
         )
 
 
-class PenaltyViewTest(TestCase):
+class PenaltyViewTest1(TestCase):
     def setUp(self):
         self.client = Client()
 
@@ -394,3 +394,135 @@ class PenaltyViewTest(TestCase):
             response.json(),
             {"message": "VALUE_ERROR"}
         )
+
+
+class PenaltyViewTest2(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        Unit.objects.create(id=1, name="원")
+
+        Type.objects.create(id=2, name="PENALTY")
+
+        Role.objects.bulk_create([Role(id=1, name="관리자"), Role(id=2, name="사용자")])
+
+        user1 = User.objects.create(
+            name        ="테스트어드민유저1",
+            email       ="test2@server.com",
+            phone_number=bcrypt.hashpw("010-1111-2222".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+            role_id     =1
+        )
+
+        user2 = User.objects.create(
+            name        ="테스트커스텀유저1",
+            email       ="test1@server.com",
+            phone_number=bcrypt.hashpw("010-1111-2222".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+            role_id     =2
+        )
+
+        self.admin_token = jwt.encode(
+            {"user_id": str(user1.id)}, MY_SECRET_KEY, ALGORITHM
+        )
+
+        self.customer_token = jwt.encode(
+            {"user_id": str(user2.id)}, MY_SECRET_KEY, ALGORITHM
+        )
+
+    def tearDown(self):
+        DiscountOrPenalties.objects.all().delete()
+        User.objects.all().delete()
+        Role.objects.all().delete()
+        Type.objects.all().delete()
+        Unit.objects.all().delete()
+
+    def test_post_penalty_success(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "code": "P-P-1",
+            "number": 1000,
+            "description": "올바르지 않은 주차구역에 주차했을시 벌금금액"
+        }
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "SUCCESS"})
+
+    def test_post_penalty_unauthorized_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.customer_token}"}
+
+        data = {
+            "code": "P-P-1",
+            "number": 1000,
+            "description": "올바르지 않은 주차구역에 주차했을시 벌금금액"
+        }
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "UNAUTHORIZED"})
+
+    def test_post_penalty_invalid_code(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "code": "O-P-1",
+            "number": 1000,
+            "description": "올바르지 않은 주차구역에 주차했을시 벌금금액"
+        }
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "INVALID_CODE_FORMAT"})
+
+    def test_post_penalty_type_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "code": 1,
+            "number": 1000,
+            "description": "올바르지 않은 주차구역에 주차했을시 벌금금액"
+        }
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "TYPE_ERROR"})
+
+    def test_post_penalty_key_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {"code": "P-P-3"}
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "KEY_ERROR"})
+
+    def test_post_penalty_value_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "code"       : "P-P-3",
+            "number"     : "",
+            "description": "올바르지 않은 주차구역에 주차했을시 벌금금액"
+        }
+
+        response = self.client.post(
+            "/charges/penalty", json.dumps(data), content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "VALUE_ERROR"})
--- charges/urls.py
+++ charges/urls.py
@@ -4,5 +4,6 @@ from charges.views import DiscountView, PenaltyView
 urlpatterns = [
     path("/discount", DiscountView.as_view()),
     path("/discount/<str:discount_id>", DiscountView.as_view()),
+    path("/penalty", PenaltyView.as_view()),
     path("/penalty/<str:penalty_id>", PenaltyView.as_view()),
 ]
