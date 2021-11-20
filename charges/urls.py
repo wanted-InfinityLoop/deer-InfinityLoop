78b58bdfa5105341e496ca644fde29f369f1748c
Oderystella <ddott59@gmail.com>
2021-11-20 18:00:04 +0900

    feat: update discount or penalty condition and pass test code
    - 할인, 벌금 조건 변경
    - 할인, 벌금 조건 변경 API 테스트 코드 추가

--- charges/urls.py
+++ charges/urls.py
@@ -1,7 +1,8 @@
 from django.urls   import path
-from charges.views import DiscountView
-
+from charges.views import DiscountView, PenaltyView
 
 urlpatterns = [
     path("/discount", DiscountView.as_view()),
+    path("/discount/<str:discount_id>", DiscountView.as_view()),
+    path("/penalty/<str:penalty_id>", PenaltyView.as_view()),
 ]
