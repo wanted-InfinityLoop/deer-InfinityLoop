30370c8f1d86f6781c587f4fee99eeabf54e14bc
Oderystella <ddott59@gmail.com>
2021-11-20 18:00:04 +0900

    feat: update discount or penalty condition and pass test code
    - 할인, 벌금 조건 변경
    - 할인, 벌금 조건 변경 API 테스트 코드 추가

--- charges/tests.py
+++ charges/tests.py
@@ -6,17 +6,12 @@ from django.test import TestCase, Client
 
 from users.models   import User, Role
 from charges.models import Unit, Type, DiscountOrPenalties
-
-from my_settings import MY_SECRET_KEY, ALGORITHM
+from my_settings    import MY_SECRET_KEY, ALGORITHM
 
 
 class DiscountViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-
-        Unit.objects.create(id=1, name="%")
-
-        Type.objects.create(id=1, name="DISCOUNT")
 
         Role.objects.bulk_create([Role(id=1, name="관리자"), Role(id=2, name="사용자")])
 
@@ -42,12 +37,18 @@ class DiscountViewTest(TestCase):
             {"user_id": str(user2.id)}, MY_SECRET_KEY, ALGORITHM
         )
 
-    def tearDown(self):
+        Type.objects.create(id=1, name="discount")
+
+        Unit.objects.create(id=1, name="%")
+
+        DiscountOrPenalties.objects.create(id=1, number=30, description="주차존 반납", unit_id=1, type_id=1)
+
+    def TearDown(self):
         DiscountOrPenalties.objects.all().delete()
+        Unit.objects.all().del다ete()
+        Type.objects.all().delete()
         User.objects.all().delete()
         Role.objects.all().delete()
-        Type.objects.all().delete()
-        Unit.objects.all().delete()
 
     def test_post_discount_success(self):
         header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
@@ -140,3 +141,256 @@ class DiscountViewTest(TestCase):
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"message": "VALUE_ERROR"})
+
+    def test_put_discount_success(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : 20,
+            "description" : "할인율 변경"
+        }
+
+        response = self.client.put(
+            "/charges/discount/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {"result" : {"discount_rate" : 20, "description" : "할인율 변경"}}
+        )
+
+    def test_put_discount_unauthorized(self):
+        header = {"HTTP_Authorization": f"Bearer {self.customer_token}"}
+
+        data = {
+            "number" : 20,
+            "description" : "할인율 변경"
+        }
+
+        response = self.client.put(
+            "/charges/discount/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(),
+            {"message": "UNAUTHORIZED"}
+        )
+    
+    def test_put_discount_does_not_exist(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : 20,
+            "description" : "할인율 변경"
+        }
+
+        response = self.client.put(
+            "/charges/discount/2",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {"message": "DISCOUNT_DOES_NOT_EXIST"}
+        )
+
+    def test_put_discount_key_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "description" : "할인율 변경"
+        }
+
+        response = self.client.put(
+            "/charges/discount/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {"message": "KEY_ERROR"}
+        )
+
+    def test_put_discount_value_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : "abc",
+            "description" : "할인율 변경"
+        }
+
+        response = self.client.put(
+            "/charges/discount/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {"message": "VALUE_ERROR"}
+        )
+
+
+class PenaltyViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        Role.objects.create(id=1, name="관리자")
+        Role.objects.create(id=2, name="사용자")
+
+        user1 = User.objects.create(
+            email="abc@gmail.com",
+            phone_number = bcrypt.hashpw("010-2345-3544".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+            name="관리자",
+            role_id=1,
+        )
+
+        self.admin_token = jwt.encode(
+            {"user_id": str(user1.id)}, MY_SECRET_KEY, ALGORITHM
+        )
+
+        user2 = User.objects.create(
+            id="bf7d24f136804c027e35fa9a72cc8b67",
+            email="abc3@gmail.com",
+            phone_number = bcrypt.hashpw("010-2345-3544".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+            name="유저1",
+            role_id=2,
+        )
+
+        self.customer_token = jwt.encode(
+            {"user_id": str(user2.id)}, MY_SECRET_KEY, ALGORITHM
+        )
+
+        Type.objects.create(id=1, name="penalty")
+
+        Unit.objects.create(id=1, name="원")
+
+        DiscountOrPenalties.objects.create(id=1, number=6000, description="반납 금지 지역 반납", unit_id=1, type_id=1)
+
+    def TearDown(self):
+        DiscountOrPenalties.objects.all().delete()
+        Unit.objects.all().delete()
+        Type.objects.all().delete()
+        User.objects.all().delete()
+        Role.objects.all().delete()
+
+    def test_put_penalty_success(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : 6000,
+            "description" : "반납 금지 지역 반납"
+        }
+
+        response = self.client.put(
+            "/charges/penalty/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {"result" : {"penalty" : 6000,
+                "description" : "반납 금지 지역 반납"}}
+        )
+
+    def test_put_penalty_unauthorized(self):
+        header = {"HTTP_Authorization": f"Bearer {self.customer_token}"}
+
+        data = {
+            "number" : 6000,
+            "description" : "반납 금지 지역 반납"
+        }
+
+        response = self.client.put(
+            "/charges/penalty/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(),
+            {"message": "UNAUTHORIZED"}
+        )
+    
+    def test_put_penalty_does_not_exist(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : 6000,
+            "description" : "반납 금지 지역 반납"
+        }
+
+        response = self.client.put(
+            "/charges/penalty/2",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {"message": "PENALTY_DOES_NOT_EXIST"}
+        )
+
+    def test_put_penalty_key_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "description" : "반납 금지 지역 반납"
+        }
+
+        response = self.client.put(
+            "/charges/penalty/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {"message": "KEY_ERROR"}
+        )
+
+    def test_put_penalty_value_error(self):
+        header = {"HTTP_Authorization": f"Bearer {self.admin_token}"}
+
+        data = {
+            "number" : "abc",
+            "description" : "반납 금지 지역 반납"
+        }
+
+        response = self.client.put(
+            "/charges/penalty/1",
+            json.dumps(data),
+            content_type="application/json",
+            **header
+            )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {"message": "VALUE_ERROR"}
+        )
--- charges/views.py
+++ charges/views.py
@@ -1,12 +1,11 @@
 import json
 
-from django.http            import JsonResponse
-from django.views           import View
-from django.db.utils        import IntegrityError
+from django.http     import JsonResponse
+from django.views    import View
+from django.db.utils import IntegrityError
 
-from users.models   import Role
-from charges.models import Unit, Type, DiscountOrPenalties
-
+from users.models    import Role
+from charges.models  import Unit, Type, DiscountOrPenalties
 from core.utils      import login_decorator
 from core.validation import code_validator
 
@@ -47,3 +46,65 @@ class DiscountView(View):
 
         except Unit.DoesNotExist:
             return JsonResponse({"message": "UNIT_NOT_EXIST"}, status=404) 
+    
+    @login_decorator
+    def put(self, request, discount_id):
+        try: 
+            if request.user.role_id != Role.Type.ADMIN.value:
+                return JsonResponse({"message": "UNAUTHORIZED"}, status = 401)
+            
+            data = json.loads(request.body)
+
+            discount_entity = DiscountOrPenalties.objects.get(id=discount_id)
+
+            if data["number"]:
+                discount_entity.number = data["number"]
+
+            if data["description"]:
+                discount_entity.description = data["description"]
+
+            discount_entity.save()
+            
+            return JsonResponse({"result" : {"discount_rate" : discount_entity.number,
+                "description" : discount_entity.description}}, status=200)
+
+        except DiscountOrPenalties.DoesNotExist:
+            return JsonResponse({"message": "DISCOUNT_DOES_NOT_EXIST"}, status=404)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        
+        except ValueError:
+            return JsonResponse({"message": "VALUE_ERROR"}, status=400)
+
+
+class PenaltyView(View):
+    @login_decorator
+    def put(self, request, penalty_id):
+        try: 
+            if request.user.role_id != Role.Type.ADMIN.value:
+                return JsonResponse({"message": "UNAUTHORIZED"}, status = 401)
+            
+            data = json.loads(request.body)
+
+            penalty_entity = DiscountOrPenalties.objects.get(id=penalty_id)
+
+            if data["number"]:
+                penalty_entity.number = data["number"]
+
+            if data["description"]:
+                penalty_entity.description = data["description"]
+
+            penalty_entity.save()
+            
+            return JsonResponse({"result" : {"penalty" : penalty_entity.number,
+                "description" : penalty_entity.description}}, status=200)
+
+        except DiscountOrPenalties.DoesNotExist:
+            return JsonResponse({"message": "PENALTY_DOES_NOT_EXIST"}, status=404)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        
+        except ValueError:
+            return JsonResponse({"message": "VALUE_ERROR"}, status=400)
