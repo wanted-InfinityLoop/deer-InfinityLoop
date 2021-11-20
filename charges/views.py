75f5a9564e29d8f31267c2824997aee8d9829d7e
YeJun <hayejun1013@naver.com>
2021-11-20 23:11:24 +0900

    feat: add penalty charge post view function
    - 벌금요금 추가 기능 구현
    - 벌금요금 추가 기능 테스트 코드 구현

--- charges/views.py
+++ charges/views.py
@@ -18,20 +18,18 @@ class DiscountView(View):
                 return JsonResponse({"message": "UNAUTHORIZED"}, status=401)
 
             data = json.loads(request.body)
-
-            if not data["code"] or not data["number"] or not data["description"]:
-                return JsonResponse({"message": "VALUE_ERROR"}, status=400)
+            number = int(data["number"])
 
             if not code_validator("D", data["code"]):
                 return JsonResponse({"message": "INVALID_CODE_FORMAT"}, status=401)
 
             DiscountOrPenalties.objects.create(
-                id         =data["code"],
-                number     =data["number"],
-                description=data["description"],
-                unit       =Unit.objects.get(name="%"),
-                type_id    =Type.Type.DISCOUNT.value
-            )
+                    id         =data["code"],
+                    number     =number,
+                    description=data["description"],
+                    unit       =Unit.objects.get(name="%"),
+                    type_id    =Type.Type.DISCOUNT.value
+                )
 
             return JsonResponse({"message": "SUCCESS"}, status=201)
 
@@ -43,6 +41,9 @@ class DiscountView(View):
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except ValueError:
+            return JsonResponse({"message": "VALUE_ERROR"}, status=400)
 
         except Unit.DoesNotExist:
             return JsonResponse({"message": "UNIT_NOT_EXIST"}, status=404) 
@@ -79,6 +80,43 @@ class DiscountView(View):
 
 
 class PenaltyView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            if request.user.role_id != Role.Type.ADMIN.value:
+                return JsonResponse({"message": "UNAUTHORIZED"}, status=401)
+
+            data = json.loads(request.body)
+            number = int(data["number"])
+
+            if not code_validator("P", data["code"]):
+                return JsonResponse({"message": "INVALID_CODE_FORMAT"}, status=401)
+
+            DiscountOrPenalties.objects.create(
+                    id         =data["code"],
+                    number     =number,
+                    description=data["description"],
+                    unit       =Unit.objects.get(name="원"),
+                    type_id    =Type.Type.PENALTY.value
+                )
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except IntegrityError:
+            return JsonResponse({"message": "DUPLICATE_DATA"}, status=409)
+
+        except TypeError:
+            return JsonResponse({"message": "TYPE_ERROR"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except ValueError:
+            return JsonResponse({"message": "VALUE_ERROR"}, status=400)
+
+        except Unit.DoesNotExist:
+            return JsonResponse({"message": "UNIT_NOT_EXIST"}, status=404) 
+
     @login_decorator
     def put(self, request, penalty_id):
         try: 
